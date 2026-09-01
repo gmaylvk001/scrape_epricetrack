57a891deb5831aa6101377c3b5c9d204920371ce
AjithMDev <ajithajith33398@gmail.com>
2026-09-01 18:46:44 +0530

    reliance changes 2

--- reliancedigital.js
+++ reliancedigital.js
@@ -73,7 +73,7 @@ async function reliancedigitalScraper(req, res) {
         });
 
         browser = await puppeteer.launch({
-            headless: false,
+            headless: 'new',
             executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
             args: [
                 '--no-sandbox',
